e189f687c2d59a403767805184a486ea4e74b41a
MO Nasser <39590845+MU-N@users.noreply.github.com>
2021-12-11 14:30:23 +0200

    adding slide effect

--- Assets/__ Super Business Woman/Scripts/Core/PlayerController.cs
+++ Assets/__ Super Business Woman/Scripts/Core/PlayerController.cs
@@ -6,30 +6,66 @@ namespace Nasser.SBW.Core
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] float moveSpeed;
 
 
-        Rigidbody rb;
-        Animator animator;
+
+        [SerializeField] private float speed = 0.5f, computerSpeed, dir = -1f;
+        [SerializeField] private float mapWidth = 2.5f;
+
+
+        private bool touching = false;
+
+
+        private float positionX, positionY;
+
+        private Vector3 originPos;
+
+        private Rigidbody rb;
+        private Animator animator;
+
+        private Touch initTouch = new Touch();
         void Start()
         {
-            rb = GetComponent<Rigidbody>(); 
-            animator = GetComponent<Animator>();
+            //Initializations
+            rb = GetComponent<Rigidbody>();
+            positionX = 0f;
+            positionY = transform.localPosition.y;
+            originPos = transform.localPosition;
         }
 
         void Update()
         {
-            GetInput();
-        }
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)        //if finger touches the screen
+                {
+                    if (touching == false)
+                    {
+                        touching = true;
+                        initTouch = touch;
+                    }
+                }
+                else if (touch.phase == TouchPhase.Moved)       //if finger moves while touching the screen
+                {
+                    float deltaX = initTouch.position.x - touch.position.x;
+                    positionX -= deltaX * Time.deltaTime * speed * dir;
+                    positionX = Mathf.Clamp(positionX, -mapWidth, mapWidth);      //to set the boundaries of the player's position
+                    transform.localPosition = new Vector3(positionX, positionY, 0f);
+                    initTouch = touch;
+                }
+                else if (touch.phase == TouchPhase.Ended)       //if finger releases the screen
+                {
+                    initTouch = new Touch();
+                    touching = false;
+                }
+            }
 
-
-        private void FixedUpdate()
-        {
-            
-        }
-        void GetInput()
-        {
-            
+            //if you play on computer---------------------------------
+            float x = Input.GetAxis("Horizontal") * Time.deltaTime * computerSpeed;     //you can move by pressing 'a' - 'd' or the arrow keys
+            Vector3 newPosition = rb.transform.localPosition + Vector3.right * x;
+            newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+            transform.localPosition = newPosition;
+            //--------------------------------------------------------
         }
 
 
