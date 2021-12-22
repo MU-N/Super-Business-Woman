0495ad72793d35649104180f9224f404e06043ae
MO Nasser <39590845+MU-N@users.noreply.github.com>
2021-12-23 01:38:46 +0200

    update money rotation

--- Assets/__ Super Business Woman/Scripts/Intercatable/Money.cs
+++ Assets/__ Super Business Woman/Scripts/Intercatable/Money.cs
@@ -5,13 +5,20 @@ using DG.Tweening;
 
 namespace Nasser.SBW.Core
 {
+    
     public class Money : MonoBehaviour,IIntercatable
     {
+        [SerializeField] private Vector3 rotationTraget;
+        
+        private void Start()
+        {
+            //transform.DOLocalRotate(rotationTraget, 1).SetEase(Ease.InSine).SetLoops(-1);
+        }
         [ContextMenu("InterAct")]
         public void Interact()
         {
 
-            transform.DORotate(Vector3.up, 1).SetEase(Ease.InSine).SetLoops(-1);
+            transform.DORotate(rotationTraget, 2.5F,RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1,LoopType.Restart);
         }
 
        
